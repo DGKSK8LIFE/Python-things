8cd2ced0f94538e629cde4d7139d9210297bcb08
DGKSK8LIFE <45473363+DGKSK8LIFE@users.noreply.github.com>
2019-01-25 11:30:18 -0600

    Update while_true.py

--- while_true.py
+++ while_true.py
@@ -1,7 +1,7 @@
 x = 1
 
 while True:
-    print("To infinity and beyond! We're getting close, on  now!" + str(x))
+    print("To infinity and beyond! We're getting close, on " + str(x)) + "now ! "
     x += 1
     if x > 500:
         print("We're done now!")
