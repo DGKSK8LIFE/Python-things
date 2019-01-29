0728d9469651d98f728a7348573e922bce4a7b09
DGKSK8LIFE <45473363+DGKSK8LIFE@users.noreply.github.com>
2019-01-29 17:52:50 -0600

    Update temp.py

--- temp.py
+++ temp.py
@@ -2,19 +2,18 @@ question = input("Are you going out?")
 
 
 
-while question == "yes" or "maybe ":
+while question == "yes" or "maybe":
   if question == "no":
     break
   temperature = float(input("What is the temperature? "))
-  if temperature in range(60, 70):
+  if temperature >= 60 and temperature <= 70:
     print("Wear a light jacket or long sleeve shirt. ")
-  elif temperature in range(50, 59):
+  elif temperature >= 50 and temperature <= 59:
     print("Wear a light vest under your jacket. ")
-  elif temperature in range(40, 49):
+  elif temperature >= 40 and temperature <= 49:
     print("Wear a sweater under your jacket, wear gloves and bring your hat. ")
   elif temperature < 40:
     print("It's too cold outside, watch some netflix. ")
   elif temperature > 70:
     print("Get some sunscreen and go to the pool. ")
   break
-  
