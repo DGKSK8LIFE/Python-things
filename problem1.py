5b43d102d42181db1363a0bdfc678219f179d6b1
DGKSK8LIFE <45473363+DGKSK8LIFE@users.noreply.github.com>
2019-02-04 17:45:59 -0600

    Add files via upload

---
+++ problem1.py
@@ -0,0 +1,14 @@
+sum = 0 
+for i in range(0, 1000):
+        if i % 3 == 0 or i % 5 == 0:
+                sum += i 
+              
+print("The sum of all the multiples is " + str(sum))    
+
+
+
+                
+        
+# The sum of 0 numbers is 0
+# The product of 0 numbers is 1 
+
