85ba262536faaec0ad1bceff72fca82a964cdf2d
DGKSK8LIFE <45473363+DGKSK8LIFE@users.noreply.github.com>
2019-06-20 08:44:54 -0500

    Update mysequence.py

--- mysequence.py
+++ mysequence.py
@@ -1,11 +1,4 @@
 def mysequence():
-    x = 0
-    while x > -1:
-        x += 1
-        y = (x - 1) + (x)
-        if y >= 500:
-            break
-        else:
-            print(y)
-
+    for i in range(0, 500, 2):
+        print(i)
 mysequence()
